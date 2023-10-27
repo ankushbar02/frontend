9a501458859dd919e653fbe9efa3c9537e03c87a
ankushbar02 <ankushbar02@gmail.com>
2023-10-28 01:18:56 +0530

    final push

--- src/components/Read/Read.js
+++ src/components/Read/Read.js
@@ -11,7 +11,8 @@ export default function Read() {
   const [userName, setUserName] = useState("");
   const [error, setError] = useState("");
   const [data, setData] = useState([]);
-
+  const [text, seTtext] = useState("");
+  const [head, setHead] = useState("");
   const jwt = Cookies.get().jwt;
 
   const today = new Date();
@@ -109,6 +110,17 @@ export default function Read() {
     Cookies.remove("jwt");
     navigate("/");
   };
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setHead("");
+    seTtext("");
+  };
 
   return (
     <div className="">
@@ -140,13 +152,22 @@ export default function Read() {
           </div>
         </div>
 
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close btn btn-dark" onClick={closeModal}>
+                &times;
+              </span>
+              <h2>{head}</h2>
+              <p>{text}</p>
+            </div>
+          </div>
+        )}
+
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
           {data?.map((ele) => (
             <div key={ele._id} className="col  opacity-100 ">
-              <div
-               
-                className=" cards p-3 rounded-3 mb-2"
-              >
+              <div className=" cards p-3 rounded-3 mb-2">
                 <div className="card-body">
                   <div className=" d-flex flex-wrap justify-content-between border-bottom border-dark ">
                     <h6 className="pb-2 "> {ele.tittle}</h6>
@@ -154,13 +175,23 @@ export default function Read() {
                       Created: {getDate(ele.createdAt)}
                     </small>
                   </div>
-                  <p className="card-text">
-                    <code className="fs-5">{ele.note}</code>
+                  <p className="card-text note">
+                    <code className="fs-5">{ele.note.slice(0,200)+"..."}</code>
                   </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
+                      <button
+                        className="btn btn-dark"
+                        onClick={() => {
+                          openModal();
+                          seTtext(ele.note);
+                          setHead(ele.tittle);
+                        }}
+                      >
+                        read
+                      </button>
                       <Link
-                        className="btn btn-sm btn-outline-secondary"
+                        className="btn btn-light btn-outline-secondary"
                         to={`/update/${ele._id}`}
                       >
                         <span className="material-symbols-outlined fs-6">
@@ -169,7 +200,7 @@ export default function Read() {
                       </Link>
                       <button
                         type="button"
-                        className="btn btn-sm   text-danger btn-outline-secondary"
+                        className="btn btn-danger    text-light btn-outline-secondary"
                         onClick={() => handleDelete(ele._id)}
                       >
                         <span className="material-symbols-outlined fs-6">
