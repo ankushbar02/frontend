14d14084e667fc420b99f1761f7a49fd214e361f
ankushbar02 <ankushbar02@gmail.com>
2023-10-28 15:39:07 +0530

    lazy loader

--- src/components/Read/Read.js
+++ src/components/Read/Read.js
@@ -122,6 +122,9 @@ export default function Read() {
     seTtext("");
   };
 
+  const numberOfTimes = 4;
+  const codeArray = new Array(numberOfTimes).fill(null);
+
   return (
     <div className="read">
       {error && <div className="alert alert-danger fixed-bottom">{error}</div>}
@@ -165,63 +168,97 @@ export default function Read() {
         )}
 
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-          {data?.map((ele) => (
-            <div key={ele._id} className="col  opacity-100 ">
-              <div className=" cards p-3 rounded-3 mb-2">
-                <div className="card-body">
-                  <div className=" d-flex flex-wrap justify-content-between border-bottom border-dark ">
-                    <h6 className="pb-2 "> {ele.tittle}</h6>
-                    <small className="text-light-emphasis sma">
-                      Created: {getDate(ele.createdAt)}
-                    </small>
-                  </div>
-                  <p className="card-text note">
-                    <code className="fs-5 description">
-                      {ele.note.length < 200
-                        ? ele.note
-                        : ele.note.slice(0, 200) + "..."}
-                    </code>
-                  </p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button
-                        className="btn text-light  btn-outline-dark"
-                        onClick={() => {
-                          openModal();
-                          seTtext(ele.note);
-                          setHead(ele.tittle);
-                        }}
-                      >
-                        <span className="material-symbols-outlined span-icon span-icon-read ">
-                          notes
-                        </span>
-                      </button>
-                      <Link
-                        className="btn text-light  btn-outline-dark"
-                        to={`/update/${ele._id}`}
-                      >
-                        <span className="material-symbols-outlined span-icon span-icon-edit">
-                          edit_note
-                        </span>
-                      </Link>
-                      <button
-                        type="button"
-                        className="btn  text-danger   btn-outline-dark"
-                        onClick={() => handleDelete(ele._id)}
-                      >
-                        <span className="material-symbols-outlined span-icon span-icon-delete">
-                          delete_sweep
-                        </span>
-                      </button>
+          {data.length === 0 ? (
+            <div>
+              {codeArray.map((_, index) => (
+                <div key={index} className="col opacity-100">
+                  <div className="cards p-3 rounded-3 mb-2">
+                    <div className="card-body">
+                      <div className="d-flex flex-wrap justify-content-between border-bottom border-dark">
+                        <div  style={{ width: "60%" }} className="skeleton skeleton-text skeleton-text__body"></div>
+                        <small style={{ width: "30%" }} className="text-light-emphasis sma">
+                          <div   className="skeleton skeleton-text skeleton-text__body"></div>
+                        </small>
+                      </div>
+                      <div className="card-text note">
+                        <code className="fs-5 description">
+                          <div  className="skeleton skeleton-text skeleton-text__body"></div>
+                          <div  className="skeleton skeleton-text skeleton-text__body"></div>
+                          <div style={{ width: "40%" }}  className="skeleton skeleton-text skeleton-text__body"></div>
+                        </code>
+                      </div>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <small style={{ width: "100px" }} className="text-light-emphasis sma skeleton skeleton-text skeleton-text__body">
+                         
+                        </small>
+                        <small style={{ width: "30%" }} className="text-light-emphasis sma skeleton skeleton-text skeleton-text__body">
+                         
+                        </small>
+                      </div>
                     </div>
-                    <small className="text-light-emphasis sma">
-                      updated: {getDate(ele.updatedAt)}
-                    </small>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            data.map((ele) => (
+              <div key={ele._id} className="col  opacity-100 ">
+                <div className=" cards p-3 rounded-3 mb-2">
+                  <div className="card-body">
+                    <div className=" d-flex flex-wrap justify-content-between border-bottom border-dark ">
+                      <h6 className="pb-2 "> {ele.tittle}</h6>
+                      <small className="text-light-emphasis sma">
+                        Created: {getDate(ele.createdAt)}
+                      </small>
+                    </div>
+                    <p className="card-text note">
+                      <code className="fs-5 description">
+                        {ele.note.length < 200
+                          ? ele.note
+                          : ele.note.slice(0, 200) + "..."}
+                      </code>
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          className="btn text-light  btn-outline-dark"
+                          onClick={() => {
+                            openModal();
+                            seTtext(ele.note);
+                            setHead(ele.tittle);
+                          }}
+                        >
+                          <span className="material-symbols-outlined span-icon span-icon-read ">
+                            notes
+                          </span>
+                        </button>
+                        <Link
+                          className="btn text-light  btn-outline-dark"
+                          to={`/update/${ele._id}`}
+                        >
+                          <span className="material-symbols-outlined span-icon span-icon-edit">
+                            edit_note
+                          </span>
+                        </Link>
+                        <button
+                          type="button"
+                          className="btn  text-danger   btn-outline-dark"
+                          onClick={() => handleDelete(ele._id)}
+                        >
+                          <span className="material-symbols-outlined span-icon span-icon-delete">
+                            delete_sweep
+                          </span>
+                        </button>
+                      </div>
+                      <small className="text-light-emphasis sma">
+                        updated: {getDate(ele.updatedAt)}
+                      </small>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
           <Link to={"/create"} className="btn    bot">
             <span className=" material-symbols-outlined mt-3 ">add</span>
           </Link>
